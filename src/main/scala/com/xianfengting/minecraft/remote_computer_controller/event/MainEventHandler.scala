05c9b5c70f9db523e9b74b993b2e7b3988a1a8e7
xianfengting <1142631421@qq.com>
2018-12-01 22:52:05 +0800

    Perfect the class com.xianfengting.minecraft.remote_computer_controller.event.MainEventHandler

--- src/main/scala/com/xianfengting/minecraft/remote_computer_controller/event/MainEventHandler.scala
+++ src/main/scala/com/xianfengting/minecraft/remote_computer_controller/event/MainEventHandler.scala
@@ -1,6 +1,9 @@
 package com.xianfengting.minecraft.remote_computer_controller.event
 
-import net.minecraftforge.common.MinecraftForge
+import net.minecraft.entity.player.EntityPlayerMP
+import net.minecraft.util.text.TextComponentString
+import net.minecraftforge.event.entity.living.LivingFallEvent
+import net.minecraftforge.fml.common.eventhandler.SubscribeEvent
 
 class MainEventHandler private {
 
@@ -9,6 +12,15 @@ class MainEventHandler private {
     // Do something here
   }
 
+  @SubscribeEvent
+  def onLivingFall(event: LivingFallEvent) = {
+    if (event.getEntityLiving.isServerWorld) {
+      event.getEntityLiving match {
+        case player : EntityPlayerMP =>
+          player.sendMessage(new TextComponentString(s"You fell down. Distance: ${event.getDistance}"))
+      }
+    }
+  }
 }
 
 object MainEventHandler {
