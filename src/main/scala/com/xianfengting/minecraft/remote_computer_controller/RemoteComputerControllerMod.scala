b113c36d2b0db8d72284b675c47fde16142a8e05
xianfengting <1142631421@qq.com>
2018-12-02 16:26:43 +0800

    Fixed a developing bug in Intellij IDEA (The mod can not be loaded) and a bug of loading the mod.

--- src/main/scala/com/xianfengting/minecraft/remote_computer_controller/RemoteComputerControllerMod.scala
+++ src/main/scala/com/xianfengting/minecraft/remote_computer_controller/RemoteComputerControllerMod.scala
@@ -8,11 +8,13 @@ import org.apache.logging.log4j.{LogManager, Logger}
 //@Mod(modid = "remote_computer_controller", name = "Remote Computer Controller")
 @Mod(
   modid = RemoteComputerControllerMod.MOD_ID,
-  name = RemoteComputerControllerMod.MOD_NAME)
+  name = RemoteComputerControllerMod.MOD_NAME,
+  modLanguage = "scala")
 object RemoteComputerControllerMod {
 
   final val MOD_ID = "remote_computer_controller"
   final val MOD_NAME = "Remote Computer Controller"
+
 
   protected[remote_computer_controller] var logger: Logger = _
   @SidedProxy(
@@ -20,16 +22,20 @@ object RemoteComputerControllerMod {
     serverSide = "com.xianfengting.minecraft.remote_computer_controller.CommonProxy")
   protected[remote_computer_controller] var proxy: CommonProxy = _
 
+
+  @Mod.EventHandler
   def onModPreInitialize(event: FMLPreInitializationEvent): Unit = {
-    logger = LogManager.getLogger()
-    proxy.onModPreInitialize(event)
+    RemoteComputerControllerMod.logger = LogManager.getLogger()
+    RemoteComputerControllerMod.proxy.onModPreInitialize(event)
   }
 
+  @Mod.EventHandler
   def onModInitialize(event: FMLInitializationEvent): Unit = {
-    proxy.onModInitialize(event)
+    RemoteComputerControllerMod.proxy.onModInitialize(event)
   }
 
+  @Mod.EventHandler
   def onModPostInitialize(event: FMLPostInitializationEvent): Unit = {
-    proxy.onModPostInitialize(event)
+    RemoteComputerControllerMod.proxy.onModPostInitialize(event)
   }
 }
